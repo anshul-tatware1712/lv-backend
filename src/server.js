a5aae3eea69f4d8d6ed5b487e2d99307dc648c36
anshul-tatware1712 <anshultatware01@gmail.com>
2025-11-11 13:11:21 +0530

    cors fix

--- src/server.js
+++ src/server.js
@@ -7,7 +7,39 @@ import userRoutes from "./route/userRouter.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://lv-assignment-frontend.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:8000",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "sec-ch-ua",
+    "sec-ch-ua-platform",
+    "sec-ch-ua-mobile",
+    "User-Agent",
+  ],
+  maxAge: 86400,
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 
 connectDB();
