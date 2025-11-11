7234663f960566d043b2fe870f445ac8f9282139
anshul-tatware1712 <anshultatware01@gmail.com>
2025-11-11 13:13:55 +0530

    buidl fix

--- src/server.js
+++ src/server.js
@@ -8,37 +8,29 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = [
-  "https://lv-assignment-frontend.vercel.app",
-  "http://localhost:3000",
-  "http://localhost:8000",
+  'https://lv-assignment-frontend.vercel.app',
+  'http://localhost:3000',
+  'http://localhost:8000'
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-
+    
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "sec-ch-ua",
-    "sec-ch-ua-platform",
-    "sec-ch-ua-mobile",
-    "User-Agent",
-  ],
-  maxAge: 86400,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'sec-ch-ua', 'sec-ch-ua-platform', 'sec-ch-ua-mobile', 'User-Agent'],
+  maxAge: 86400
 };
 
+// This middleware handles both regular requests AND preflight OPTIONS
 app.use(cors(corsOptions));
-
-app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
